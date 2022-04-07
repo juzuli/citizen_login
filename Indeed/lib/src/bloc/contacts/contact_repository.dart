625b95619c983a71e1868610cf8f72d492521bff
MOHAMMED JUZULI M P <99029289+juzuli@users.noreply.github.com>
2022-04-07 17:27:35 +0530

    Delete Indeed directory

--- Indeed/lib/src/bloc/contacts/contact_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/services.dart' show rootBundle;
-
-class ContactRepository{
-
-  Future<String> loadAsset() async{
-    return await rootBundle.loadString("assets/data/records.json");
-  }
-
-}
